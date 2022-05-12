dde4aad0ed76712bbeb11d2133b0ff8e9dcaf18f
APN-Pucky <APN-Pucky@noreply.github.com>
2022-05-12 18:47:20 +0200

    Init with setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("Readme.md", "r") as fh:
+    long_description = fh.read()
+
+requirements = [
+    "termcolor",
+    "deep_translator",
+]
+
+
+setuptools.setup(
+    name="autotext",
+    setup_requires=['setuptools-git-versioning'],
+    author="APN",
+    author_email="APN-Pucky@no-reply.github.com",
+    description="auto rewrite text",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/APN-Pucky/autotext", #TODO change
+    #packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=requirements,
+    version_config={
+        "template": "{tag}",
+        "dev_template": "{tag}.{ccount}",
+        "dirty_template": "{tag}.{ccount}+dirty",
+        "starting_version": "0.0.0",
+        "version_callback": None,
+        "version_file": None,
+        "count_commits_from_version_file": False
+    },
+    scripts=['autotext'],
+    python_requires='>=3.6',
+)
