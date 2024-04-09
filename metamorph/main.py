ab61f212e201fd151e5ecc880a84cc0b0366900d
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2024-04-09 17:39:10 +0200

    Add merge argument

--- metamorph/main.py
+++ metamorph/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import argparse
 import sys
+
 # readline might not be available on all platforms
 try:
     import readline
@@ -9,6 +10,7 @@ except ImportError:
 # colorama might not be available on all platforms
 try:
     import colorama
+
     from metamorph.util import get_edits_string
 except ImportError:
     get_edits_string = lambda old, new: new
@@ -18,40 +20,139 @@ from metamorph.config import Config
 from metamorph.handler import generate_alternatives
 
 
-
-
 def __main__():
     """
     Main function.
     """
-    parser = argparse.ArgumentParser(description='Metamorph-' + str(metamorph.version) + ' text by passing through several translators.')
-    parser.add_argument("-gs","--goal-start",type=str, help="initial and final language", default="en")
-    parser.add_argument("-g","--goal",type=str, help="final language", default=None)
-    parser.add_argument("-s","--start",type=str, help="initial language", default=None)
-    parser.add_argument("-t","--translator", type=str,help="default translator (from deep_translator: GoogleTranslator, PonsTranslator, LingueeTranslator, MyMemoryTranslator, DeeplTranslator, ... )",default="GoogleTranslator")
-    parser.add_argument("-l","--languages",type=str,nargs='+', help="list of intermediate languages (for more variation choose inherently different languages).", default=["de","fr","es"])
+    parser = argparse.ArgumentParser(
+        description="Metamorph-"
+        + str(metamorph.version)
+        + " text by passing through several translators."
+    )
+    parser.add_argument(
+        "-gs", "--goal-start", type=str, help="initial and final language", default="en"
+    )
+    parser.add_argument("-g", "--goal", type=str, help="final language", default=None)
+    parser.add_argument(
+        "-s", "--start", type=str, help="initial language", default=None
+    )
+    parser.add_argument(
+        "-t",
+        "--translator",
+        type=str,
+        help="default translator (from deep_translator: GoogleTranslator, PonsTranslator, LingueeTranslator, MyMemoryTranslator, DeeplTranslator, ... )",
+        default="GoogleTranslator",
+    )
+    parser.add_argument(
+        "-l",
+        "--languages",
+        type=str,
+        nargs="+",
+        help="list of intermediate languages (for more variation choose inherently different languages).",
+        default=["de", "fr", "es"],
+    )
 
-    parser.add_argument("-i","--interactive",action='store_true',help="Enable interactive mode. Will prompt for input.",default=True)
-    parser.add_argument("-ni","--non-interactive",action='store_false',help="Parse stdin, without input prompts.",dest="interactive")
-    parser.add_argument("-a","--all-in-one",action='store_true', help="Instead of parsing input line by line, parse all text at once",default=False)
+    parser.add_argument(
+        "-i",
+        "--interactive",
+        action="store_true",
+        help="Enable interactive mode. Will prompt for input.",
+        default=True,
+    )
+    parser.add_argument(
+        "-ni",
+        "--no-interactive",
+        "--non-interactive",
+        action="store_false",
+        help="Parse stdin, without input prompts.",
+        dest="interactive",
+    )
+    parser.add_argument(
+        "-a",
+        "--all-in-one",
+        action="store_true",
+        help="Instead of parsing input line by line, parse all text at once",
+        default=False,
+    )
 
-    parser.add_argument("-c","--config", type=str,help="load config from a file",default=None)
-    parser.add_argument("-v","--verbose", type=bool,help="print error messages instead of skipping them",default=False)
-    parser.add_argument("-q","--quiet", type=bool,help="suppress error messages",default=False)
+    parser.add_argument(
+        "-m",
+        "--merge",
+        action="store_true",
+        help="merge all translations into one",
+        default=False,
+    )
 
-    parser.add_argument("--colour",action='store_true' ,help="show coloured differences",default=True)
-    parser.add_argument("-nc","--no-colour",dest='colour',action='store_false' ,help="don't show coloured differences")
+    parser.add_argument(
+        "-c", "--config", type=str, help="load config from a file", default=None
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        type=bool,
+        help="print error messages instead of skipping them",
+        default=False,
+    )
+    parser.add_argument(
+        "-q", "--quiet", type=bool, help="suppress error messages", default=False
+    )
 
-    parser.add_argument("--background-colour",action='store_true' ,help="show coloured deletions",default=True)
-    parser.add_argument("-nbc","--no-background-colour",dest='background_colour',action='store_false' ,help="don't show coloured deletions")
+    parser.add_argument(
+        "--colour", action="store_true", help="show coloured differences", default=True
+    )
+    parser.add_argument(
+        "-nc",
+        "--no-colour",
+        dest="colour",
+        action="store_false",
+        help="don't show coloured differences",
+    )
 
+    parser.add_argument(
+        "--background-colour",
+        action="store_true",
+        help="show coloured deletions",
+        default=True,
+    )
+    parser.add_argument(
+        "-nbc",
+        "--no-background-colour",
+        dest="background_colour",
+        action="store_false",
+        help="don't show coloured deletions",
+    )
 
-    parser.add_argument("-sd", "--show-diagrams", action='store_true', help="print all diagrams",default=False )
-    parser.add_argument("-sdi", "--show-diagram-init", action='store_false', help="print the diagram translation flow",default=True)
-    parser.add_argument("-hdi", "--hide-diagram-init", dest='show_diagram_init',action='store_false', help="don't print the diagram translation flow")
-    parser.add_argument("-sdr", "--show-diagram-result", action='store_true', help="print the diagram including intermediate translations",default=False )
+    parser.add_argument(
+        "-sd",
+        "--show-diagrams",
+        action="store_true",
+        help="print all diagrams",
+        default=False,
+    )
+    parser.add_argument(
+        "-sdi",
+        "--show-diagram-init",
+        action="store_false",
+        help="print the diagram translation flow",
+        default=True,
+    )
+    parser.add_argument(
+        "-hdi",
+        "--hide-diagram-init",
+        dest="show_diagram_init",
+        action="store_false",
+        help="don't print the diagram translation flow",
+    )
+    parser.add_argument(
+        "-sdr",
+        "--show-diagram-result",
+        action="store_true",
+        help="print the diagram including intermediate translations",
+        default=False,
+    )
 
     args = parser.parse_args()
+
     if args.colour:
         colorama.init()
     goal = args.goal_start
@@ -59,17 +160,26 @@ def __main__():
     if args.goal is not None:
         goal = args.goal
     if args.start is not None:
-        start= args.start
+        start = args.start
 
     conf = None
     if args.config is not None:
-        conf = Config(args.config, goal=goal,start= start,translator=args.translator)
+        conf = Config(args.config, goal=goal, start=start, translator=args.translator)
     else:
-        conf = Config(flow={l:None for l in args.languages }, goal=goal,start= start,translator=args.translator)
-        
+        conf = Config(
+            flow={l: None for l in args.languages},
+            goal=goal,
+            start=start,
+            translator=args.translator,
+        )
+
+    if args.merge and not args.interactive:
+        print("Merge is only available in interactive mode.")
+        exit(-1)
+
     if args.interactive and (args.show_diagrams or args.show_diagram_init):
         print("Loaded translation diagram:")
-        print(conf.str_diagram(nodes="language" ,arrows="translator_short"))
+        print(conf.str_diagram(nodes="language", arrows="translator_short"))
 
     if args.interactive:
         try:
@@ -79,13 +189,20 @@ def __main__():
                 s = generate_alternatives(to_translate, conf)
                 for tmp_text in s:
                     if args.colour:
-                        print(get_edits_string(to_translate,tmp_text,conf.color,conf.on_color if args.background_colour else None))
+                        print(
+                            get_edits_string(
+                                to_translate,
+                                tmp_text,
+                                conf.color,
+                                conf.on_color if args.background_colour else None,
+                            )
+                        )
                     else:
                         print(tmp_text)
                 print()
                 if args.show_diagrams or args.show_diagram_result:
                     print("Diagram:")
-                    print(conf.str_diagram(nodes="result",arrows="language"))
+                    print(conf.str_diagram(nodes="result", arrows="language"))
         except KeyboardInterrupt:
             sys.exit(0)
     else:
@@ -96,8 +213,8 @@ def __main__():
         for line in lines:
             s = generate_alternatives(line, conf)
             if results is None:
-                results = [""]*len(s)
-            for i,tmp_text in enumerate(s):
+                results = [""] * len(s)
+            for i, tmp_text in enumerate(s):
                 results[i] = results[i] + tmp_text + "\n"
         for tmp_text in results:
             print(tmp_text)
